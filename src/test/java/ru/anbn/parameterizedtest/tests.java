645ab943cc33f8a3412c1e3ca4ebd561d204802b
GTemp <anbngm@gmail.com>
2022-02-27 17:25:07 +0500

    Homework for checking

--- src/test/java/ru/anbn/parameterizedtest/tests.java
+++ src/test/java/ru/anbn/parameterizedtest/tests.java
@@ -1,24 +1,28 @@
 package ru.anbn.parameterizedtest;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class tests {
 // Открыть сайт https://ip-calculator.ru
 // Ввод IP адреса и маски, расчет параметров подсети, проверка корректности выходных данных
 
-// Тестовые данные (проверим только часть параметров)
-// -- Проверить для запроса {"10.78.253.10", "29 - 255.255.255.248"}
-// -- Ожидаемый результат {"29", "0.0.0.7", "10.78.253.8"}
+    @BeforeAll
+    static void beforeAll() {
+        browserPosition = "0x0";
+        browserSize = "1920x1080";
+    }
 
     @BeforeEach
     void precondition() {
-        open("https://ip-calculator.ru/#!ip=10.78.253.10/29");
+        open("https://ip-calculator.ru");
     }
 
     @AfterEach
@@ -28,10 +32,11 @@ public class tests {
 
     // параметризованный тест
     @CsvSource(value = {
-            // первые два параметра - входные данные, последние три - выходные. 29 и 28 прописали два раза
-            // для удобства понимания данных
+            // первые два параметра - входные данные, последние три - выходные
             "10.78.253.10|29|29|0.0.0.7|10.78.253.8",
-            "10.78.250.55|28|28|0.0.0.15|10.78.250.48"
+            "10.78.250.55|28|28|0.0.0.15|10.78.250.48",
+            "192.168.0.1|24|24|0.0.0.255|192.168.0.0",
+            "135.25.26.25|21|21|0.0.7.255|135.25.24.0"
     },
             delimiter = '|'
     )
@@ -43,7 +48,6 @@ public class tests {
         // заполним маску
         $("#mask").selectOptionByValue(networkData);
         // нажимаем кнопку "Посчитать"
-        // <button type="submit" class="btn btn-primary btn-lg form-control input-lg">Подсчитать →</button>
         $("button[type='submit']").click();
 
         // проверка выходных данных
@@ -53,7 +57,6 @@ public class tests {
         $x("//*[@id=\"result\"]/div/table/tbody/tr[4]/td[2]").shouldHave(text(wildcardResult));
         // networkResult
         $("#result > div > table > tbody > tr:nth-child(5) > td:nth-child(2)").shouldHave(text(networkResult));
-
     }
 
 
